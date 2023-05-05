2f5cc90044083ea16bd690002e7f3aed6456d6e6
Haila1 <128238595+Haila1@users.noreply.github.com>
2023-05-05 16:00:34 +0300

    Add files via upload

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,15 @@
+for(int r = 0; r < N; r++)
+	// 	for(int c = 0; c < N; c++)
+	// 	{
+	// 		slashCode[r] = r + c;
+	// 		backslashCode[r] = r - c + 7;
+	// 	}
+
+	// if (solveNQueensUtil(board, 0, slashCode,
+	// 					backslashCode, rowLookup,
+	// 					slashCodeLookup,
+	// 					backslashCodeLookup) == false)
+	// {
+	// 	System.out.printf("Solution does not exist");
+	// 	return false;
+	// }
